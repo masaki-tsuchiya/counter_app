6a2bd098afbde1ad7c31b6d20d9beee2dca9951e
tsuchiya.masaki <tsuchiya.masaki@luup.co.jp>
2025-07-18 17:39:57 +0900

    new create

--- app/src/main/java/com/example/counter_app/MainScreen.kt
+++ app/src/main/java/com/example/counter_app/MainScreen.kt
@@ -1,14 +1,25 @@
 package com.example.counter_app
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.counter_app.ui.theme.CounterappTheme
@@ -16,6 +27,8 @@ import com.example.counter_app.ui.theme.CounterappTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    var counter by remember { mutableIntStateOf(0) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -26,12 +39,28 @@ fun MainScreen() {
                 title = { Text("Android Compose Demo") },
             )
         },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { counter++ }) {
+                Icon(Icons.Filled.Add, "Increment counter")
+            }
+        },
         modifier = Modifier.fillMaxSize(),
     ) { innerPadding ->
-        Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding),
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "You have pushed the button this many times:",
+            )
+            Text(
+                text = "$counter",
+                style = MaterialTheme.typography.headlineMedium,
+            )
+        }
     }
 }
 
